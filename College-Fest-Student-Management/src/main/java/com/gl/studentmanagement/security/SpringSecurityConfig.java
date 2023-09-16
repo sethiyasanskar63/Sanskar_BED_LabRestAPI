df281a5d17e8ef2afbefa622a49de9e86d0198d8
Sanskar Sethiya <sethiyasanskar63@gmail.com>
2023-09-16 23:45:02 +0530

    To use the project with MYSQL Workbench

--- College-Fest-Student-Management/src/main/java/com/gl/studentmanagement/security/SpringSecurityConfig.java
+++ College-Fest-Student-Management/src/main/java/com/gl/studentmanagement/security/SpringSecurityConfig.java
@@ -29,7 +29,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/h2-console/**");
+		//web.ignoring().antMatchers("/h2-console/**");
 	}
 
 	@Override
